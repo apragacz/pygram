c1cba83f529cd85e191e0d3ce49f469714cfd2a7
Andrzej Pragacz <andrzej.pragacz@10clouds.com>
2012-08-17 17:33:06 +0200

    lr parser update

--- pygram/parsers/base.py
+++ pygram/parsers/base.py
@@ -3,17 +3,26 @@ class LRParser(object):
     def __init__(self, action_table, transition_table, initial_state):
         self._action_table = action_table
         self._transition_table = transition_table
-        self._stack = [initial_state]
+        self._stack = [(None, initial_state)]
 
-    def parse(self, symbols):
-        for symbol in symbols:
-            current_state = self._stack[-1]
-            act = self._action_table[current_state][symbol.type]
-            if act[0]:
+    def parse(self, symbol_instances):
+        for symbol_instance in symbol_instances:
+            current_state = self._stack[-1][1]
+            act = self._action_table[current_state][symbol_instance.symbol]
+            next_state, reduction = act
+            if next_state:
                 #shift
-                self._stack.append(symbol)
-                self._stack.append(act[0])
-            else:
+                self._stack.append((symbol_instance, next_state))
+            elif reduction:
                 #reduction
-                #TODO:
-                pass
+                reduction_len = len(reduction.rule.body_symbols)
+                reduction_elems = self._stack[-reduction_len:]
+                self._stack = self._stack[:-reduction_len]
+                symbol_instances = [si for si, _ in reduction_elems]
+                reduced_symbol_instance = reduction.reduce_instances(
+                                                            symbol_instances)
+                next_state = self._transition_table[current_state][
+                                                reduced_symbol_instance.symbol]
+                self._stack.append((reduced_symbol_instance, next_state))
+            else:
+                ValueError('invalid state')
