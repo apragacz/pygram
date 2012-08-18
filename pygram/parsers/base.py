900d01d2236ff61050c4f307986b718f76b81eb1
Andrzej Pragacz <andrzej.pragacz@10clouds.com>
2012-08-18 22:40:19 +0200

    fixed lr parser

--- pygram/parsers/base.py
+++ pygram/parsers/base.py
@@ -6,13 +6,19 @@ class LRParser(object):
         self._stack = [(None, initial_state)]
 
     def parse(self, symbol_instances):
-        for symbol_instance in symbol_instances:
+        next_symbol_instances = list(symbol_instances)
+        action_table = self._action_table
+        transition_table = self._transition_table
+        while next_symbol_instances:
             current_state = self._stack[-1][1]
-            act = self._action_table[current_state][symbol_instance.symbol]
+            next_symbol_instance = next_symbol_instances[0]
+            next_symbol = next_symbol_instance.symbol
+            act = action_table[current_state][next_symbol]
             next_state, reduction = act
             if next_state:
                 #shift
-                self._stack.append((symbol_instance, next_state))
+                self._stack.append((next_symbol_instance, next_state))
+                next_symbol_instances.pop(0)
             elif reduction:
                 #reduction
                 reduction_len = len(reduction.rule.body_symbols)
@@ -21,8 +27,8 @@ class LRParser(object):
                 symbol_instances = [si for si, _ in reduction_elems]
                 reduced_symbol_instance = reduction.reduce_instances(
                                                             symbol_instances)
-                next_state = self._transition_table[current_state][
-                                                reduced_symbol_instance.symbol]
+                reduced_symbol = reduced_symbol_instance.symbol
+                next_state = transition_table[current_state][reduced_symbol]
                 self._stack.append((reduced_symbol_instance, next_state))
             else:
                 ValueError('invalid state')
