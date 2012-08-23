a68a8d3988535c09e80f1fb1f8dd114ea361335e
Andrzej Pragacz <andrzej.pragacz@10clouds.com>
2012-08-24 00:50:30 +0200

    updated slr states

--- pygram/core/states.py
+++ pygram/core/states.py
@@ -1,37 +1,23 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
+from collections import deque
+
+from .base import Immutable
 
 
-class Action(object):
-
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def __eq__(self, other):
-        pass
-
-    @abstractmethod
-    def __hash__(self):
-        pass
+class Action(Immutable):
+    pass
 
 
-class State(object):
+class State(Immutable):
 
     __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def actions(self):
-        pass
 
     @abstractmethod
     def next_states(self, action):
         pass
 
-    @abstractmethod
-    def __eq__(self, other):
-        pass
-
-    @abstractmethod
-    def __hash__(self):
+    @abstractproperty
+    def actions(self):
         pass
 
 
@@ -40,18 +26,14 @@ class DeterministicState(object):
     __metaclass__ = ABCMeta
 
     def next_states(self, action):
-        return [self.next_state(action)]
+        st = self.next_state(action)
+        if st is None:
+            return []
+        else:
+            return [st]
 
     @abstractmethod
     def next_state(self, action):
-        pass
-
-    @abstractmethod
-    def __eq__(self, other):
-        pass
-
-    @abstractmethod
-    def __hash__(self):
         pass
 
 
@@ -64,30 +46,58 @@ class MultiState(DeterministicState):
 
     __metaclass__ = ABCMeta
 
-    @abstractproperty
-    def states(self):
+    def __init__(self, atom_states):
+        self._atom_states = frozenset(self.closure(atom_states))
+
+    @abstractmethod
+    def equivalent_states(self, atom_state):
         pass
+
+    def closure(self, atom_states):
+        result_states = set(atom_states)
+        q = deque(atom_states)
+        while q:
+            s = q.popleft()
+            eq_states = self.equivalent_states(s)
+            for eq_s in eq_states:
+                if eq_s not in result_states:
+                    result_states.add(eq_s)
+                    q.append(eq_s)
+        return result_states
+
+    def constructor(self, atom_states):
+        return self.__class__(atom_states)
+
+    def next_state(self, action):
+        next_atom_states = []
+        for s in self._atom_states:
+            next_atom_states.extend(s.next_states(action))
+        return self.constructor(next_atom_states)
 
     def __eq__(self, other):
         if self.__class__ != other.__class__:
             return False
-        if self.states != other.states:
+        if self._atom_states != other._atom_states:
             return False
         return True
 
     def __hash__(self):
-        value = sum((hash(s) for s in self.states))
-        return value
+        return hash(self._atom_states)
 
+    def show(self, fun):
+        return '%s([%s])' % (self.__class__.__name__,
+                                ', '.join(map(fun, self._atom_states)))
 
-def closure(states, equivalence_states_fun):
-    result_states = set(states)
-    q = list(states)
-    while q:
-        s = q.pop(0)
-        eq_states = equivalence_states_fun(s)
-        for eq_s in eq_states:
-            if eq_s not in result_states:
-                result_states.add(eq_s)
-                q.append(eq_s)
-    return result_states
+    def __str__(self):
+        return self.show(str)
+
+    def __repr__(self):
+        return self.show(repr)
+
+    def __unicode__(self):
+        return self.show(unicode)
+
+    @classmethod
+    def generate_all(start_atom_states):
+        #TODO:
+        pass
--- pygram/parsers/states/slr.py
+++ pygram/parsers/states/slr.py
@@ -1,11 +1,26 @@
-from pygram.core.states import AtomState
+from pygram.core.states import AtomState, MultiState
 
 
-class SLRAtomState(AtomState):
+class SLRSituationState(AtomState):
     def __init__(self, head_symbol, body_previous_symbols, body_next_symbols):
         self._head_symbol = head_symbol
         self._body_previous_symbols = tuple(body_previous_symbols)
         self._body_next_symbols = tuple(body_next_symbols)
+
+    def next_state(self, action):
+        if self._body_next_symbols and self._body_next_symbols[0] == action:
+            return SLRSituationState(self._head_symbol,
+                                        self._body_previous_symbols + (action,),
+                                        self._body_next_symbols[1:])
+        else:
+            return None
+
+    @property
+    def actions(self):
+        if self._body_next_symbols:
+            return frozenset([self._body_next_symbols[0]])
+        else:
+            return frozenset([])
 
     def __eq__(self, other):
         if self.__class__ != other.__class__:
@@ -24,13 +39,54 @@ class SLRAtomState(AtomState):
         h += hash(self._body_next_symbols)
         return h
 
-    def __unicode__(self):
-        prev_sym_str = u' '.join([unicode(bs)
+    def show(self, fun):
+        prev_sym_str = ' '.join([fun(bs)
                                     for bs in self._body_previous_symbols])
-        next_sym_str = u' '.join([unicode(bs)
+        next_sym_str = ' '.join([fun(bs)
                                     for bs in self._body_next_symbols])
-        return u'%s -> %s  *  %s' % (unicode(self._head_symbol),
-                                        prev_sym_str, next_sym_str)
+        return '%s -> %s' % (fun(self._head_symbol),
+                    ' '.join(filter(None, [prev_sym_str, "(@)", next_sym_str])))
+
+    def __unicode__(self):
+        return self.show(unicode)
+
+    def __str__(self):
+        return self.show(str)
+
+    def __repr__(self):
+        return self.show(repr)
+
+    @property
+    def body_next_symbols(self):
+        return self._body_next_symbols
+
+
+class SLRState(MultiState):
+
+    def __init__(self, cf_grammar, atom_states):
+        self._cfg = cf_grammar
+        super(SLRState, self).__init__(atom_states)
+
+    def equivalent_states(self, atom_state):
+        body_next_symbols = atom_state.body_next_symbols
+        eq_states = []
+        if body_next_symbols:
+            next_bs = body_next_symbols[0]
+            for r in self._cfg.rules:
+                if r.head_symbol == next_bs:
+                    eq_states.append(SLRSituationState(r.head_symbol,
+                                            (), r.body_symbols))
+        return eq_states
+
+    def constructor(self, atom_states):
+        return self.__class__(self._cfg, atom_states)
+
+    @property
+    def actions(self):
+        result = set()
+        for s in self._atom_states:
+            result.update(s.actions)
+        return frozenset(result)
 
 
 def generate_parser_states(grammar):
