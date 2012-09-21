19a86174d1374af53db539c58a039d052ad96a2b
Andrzej Pragacz <andrzej.pragacz@10clouds.com>
2012-09-21 17:32:29 +0200

    added initial EBNF package

---
+++ pygram/ebnf/grammar_compilers.py
@@ -0,0 +1,8 @@
+
+
+class EBNFGrammarCompiler(object):
+    def __init__(self, ebnf_specification):
+        for ebnf_spec in ebnf_specification:
+            pass
+        #TODO: create rules, create grammar
+        pass
---
+++ pygram/ebnf/grammars.py
@@ -0,0 +1,6 @@
+"""
+Yo dawg, I heard you like creating grammars, so we create a EBNF grammar
+so you can create grammar while you create grammar
+"""
+
+#TODO: grammar
